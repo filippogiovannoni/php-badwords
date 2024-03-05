24d40e5eae9a85c97a49744fdd49aaa1f411a1b6
filippogiovannoni <filippogiovannonii@gmail.com>
2024-03-05 15:11:25 +0100

    Initial commit add form sent to the second file

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="index_1.php" method="get">
+        <label for="paragraph">Type a paragraph</label>
+        <input type="text" name="paragraph" id="paragraph">
+        <label for="word">Type the word to censor</label>
+        <input type="text" name="word" id="word">
+        <button type="submit">Send</button>
+    </form>
+</body>
+
+</html>
